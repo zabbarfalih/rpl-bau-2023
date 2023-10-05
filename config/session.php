b351be0752042aaca810db7bd3915196151232aa
Muhammad Zabbar Falihin <zabbarfalih@gmail.com>
2023-10-05 11:25:30 +0700

    Session & Seeder

--- config/session.php
+++ config/session.php
@@ -33,8 +33,9 @@ return [
 
     'lifetime' => env('SESSION_LIFETIME', 120),
 
-    'expire_on_close' => false,
-
+    // 'expire_on_close' => false,
+    'expire_on_close' => true,
+    
     /*
     |--------------------------------------------------------------------------
     | Session Encryption
--- database/seeders/SubmenuSeeder.php
+++ database/seeders/SubmenuSeeder.php
@@ -18,13 +18,13 @@ class SubmenuSeeder extends Seeder
         // Create a sample admin submenus
         Submenu::insert([
             [
-                'menu_id' => 2,
-                'name' => 'Users',
+                'menu_id' => 4,
+                'name' => 'Pegawai',
                 'url' => Str::slug('Users'),
                 'icon' => 'bi bi-people-fill'
             ],
             [
-                'menu_id' => 2,
+                'menu_id' => 4,
                 'name' => 'Menu & Submenu',
                 'url' => Str::slug('Menu & Submenu'),
                 'icon' => 'bi bi-menu-button-wide-fill'
