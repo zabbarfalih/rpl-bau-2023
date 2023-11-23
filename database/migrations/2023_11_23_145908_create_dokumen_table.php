d2ed82f2f3d9f1ebc676777b68fc828f7ba04968
Gholidho <222112074@stis.ac.id>
2023-11-23 15:17:44 +0700

    try db

---
+++ database/migrations/2023_11_23_145908_create_dokumen_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDokumenTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('dokumen', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('nama_pengadaan');
+            $table->date('tanggal_pengajuan');
+            $table->enum('status', ['Diajukan', 'Diterima PPK', 'Ditolak', 'Revisi', 'Diproses', 'Dilaksanakan', 'Selesai', 'Diserahkan']);
+            $table->string('memo')->nullable();
+            $table->string('kak')->nullable();
+            $table->string('undangan')->nullable();
+            $table->string('perencanaan_pengadaan')->nullable();
+            $table->string('identifikasi_kebutuhan')->nullable();
+            $table->string('ldp_dan_spesifikasi')->nullable();
+            $table->string('ikp')->nullable();
+            $table->string('pengadaan_langsung')->nullable();
+            $table->string('ssuk_sskk')->nullable();
+            $table->string('dok_penawaran_pakta_formulir')->nullable();
+            $table->boolean('pelaksana');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('dokumen');
+    }
+}
