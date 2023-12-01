5d59e147d11d6bf268fc13517132b22336160eea
hilalski <yanuar18181@gmail.com>
2023-12-01 15:10:42 +0700

    p

---
+++ app/Http/Controllers/SpjTrController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SpjTr;
+use App\Http\Requests\StoreSpjTrRequest;
+use App\Http\Requests\UpdateSpjTrRequest;
+
+class SpjTrController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreSpjTrRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreSpjTrRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\SpjTr  $spjTr
+     * @return \Illuminate\Http\Response
+     */
+    public function show(SpjTr $spjTr)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\SpjTr  $spjTr
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(SpjTr $spjTr)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateSpjTrRequest  $request
+     * @param  \App\Models\SpjTr  $spjTr
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateSpjTrRequest $request, SpjTr $spjTr)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\SpjTr  $spjTr
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SpjTr $spjTr)
+    {
+        //
+    }
+}
