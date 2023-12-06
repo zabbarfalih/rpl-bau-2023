969af1dbb67f43e4d63bb1c9b37dd1533adce00f
zabbarfalih <222112225@stis.ac.id>
2023-12-06 18:09:27 +0700

    Route Kel Yanuar

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,38 +2,53 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
     public function run()
     {
         // Create a sample admin user
-        User::create([
-            'name' => 'Muhammad Zabbar Falihin',
-            'nip' => '222112225',
-            'email' => 'admin1@example.com',
-            'email_verified_at' => now(),
-            'phone_number' => '081234567890',
-            'address' => 'Jln. Sensus 3',
-            'password' => Hash::make('password'),
-            'remember_token' => Str::random(10),
-        ]);
+        // User::create([
+        //     'name' => 'Muhammad Zabbar Falihin',
+        //     'nip' => '222112225',
+        //     'email' => 'admin1@example.com',
+        //     'email_verified_at' => now(),
+        //     'phone_number' => '081234567890',
+        //     'address' => 'Jln. Sensus 3',
+        //     'password' => Hash::make('password'),
+        //     'remember_token' => Str::random(10),
+        // ]);
 
-        User::create([
-            'name' => 'Gholidho Herda Prilasakly',
-            'nip' => '222112074',
-            'email' => 'admin2@example.com',
-            'email_verified_at' => now(),
-            'phone_number' => '085785529623',
-            'address' => 'Gg H Dawel',
-            'password' => Hash::make('password'),
-            'remember_token' => Str::random(10),
-        ]);
+        // User::create([
+        //     'name' => 'Gholidho Herda Prilasakly',
+        //     'nip' => '222112074',
+        //     'email' => 'admin2@example.com',
+        //     'email_verified_at' => now(),
+        //     'phone_number' => '085785529623',
+        //     'address' => 'Gg H Dawel',
+        //     'password' => Hash::make('password'),
+        //     'remember_token' => Str::random(10),
+        // ]);
 
-        User::factory(20)->create();
+        // User::factory(20)->create();
+
+        User::truncate();
+
+        // Path to the SQL file
+        $sqlFile = base_path('database/seeders/data/sql/users.sql');
+
+        // Check if the file exists
+        if (file_exists($sqlFile)) {
+            // Execute the SQL commands
+            DB::unprepared(file_get_contents($sqlFile));
+        } else {
+            // Log or handle the error appropriately
+            echo "SQL file not found: {$sqlFile}\n";
+        }
     }
 }
