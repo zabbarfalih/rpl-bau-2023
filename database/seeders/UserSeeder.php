6b259f59de904be388941ca0ca44837e11329b0d
zabbarfalih <222112225@stis.ac.id>
2023-12-06 18:47:20 +0700

    UserSeeder Faker

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -12,30 +12,30 @@ class UserSeeder extends Seeder
 {
     public function run()
     {
-        // Create a sample admin user
-        // User::create([
-        //     'name' => 'Muhammad Zabbar Falihin',
-        //     'nip' => '222112225',
-        //     'email' => 'admin1@example.com',
-        //     'email_verified_at' => now(),
-        //     'phone_number' => '081234567890',
-        //     'address' => 'Jln. Sensus 3',
-        //     'password' => Hash::make('password'),
-        //     'remember_token' => Str::random(10),
-        // ]);
+        Create a sample admin user
+        User::create([
+            'name' => 'Muhammad Zabbar Falihin',
+            'nip' => '222112225',
+            'email' => 'admin1@example.com',
+            'email_verified_at' => now(),
+            'phone_number' => '081234567890',
+            'address' => 'Jln. Sensus 3',
+            'password' => Hash::make('password'),
+            'remember_token' => Str::random(10),
+        ]);
 
-        // User::create([
-        //     'name' => 'Gholidho Herda Prilasakly',
-        //     'nip' => '222112074',
-        //     'email' => 'admin2@example.com',
-        //     'email_verified_at' => now(),
-        //     'phone_number' => '085785529623',
-        //     'address' => 'Gg H Dawel',
-        //     'password' => Hash::make('password'),
-        //     'remember_token' => Str::random(10),
-        // ]);
+        User::create([
+            'name' => 'Gholidho Herda Prilasakly',
+            'nip' => '222112074',
+            'email' => 'admin2@example.com',
+            'email_verified_at' => now(),
+            'phone_number' => '085785529623',
+            'address' => 'Gg H Dawel',
+            'password' => Hash::make('password'),
+            'remember_token' => Str::random(10),
+        ]);
 
-        // User::factory(20)->create();
+        User::factory(20)->create();
 
         User::truncate();
 
