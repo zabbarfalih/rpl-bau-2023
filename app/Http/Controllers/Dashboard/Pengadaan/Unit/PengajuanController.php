2495919b5085d9b7c0368f39087805bd7b7f6cb2
afnanalfian <afnan.02alf@gmail.com>
2023-12-06 19:42:17 +0700

    Routing DokumenController

--- app/Http/Controllers/Dashboard/Pengadaan/Unit/PengajuanController.php
+++ app/Http/Controllers/Dashboard/Pengadaan/Unit/PengajuanController.php
@@ -17,11 +17,9 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Storage;
 
-class PengajuanController extends Controller
-{
-    public function getColorStatus($status)
-    {
-        switch ($status) {
+class PengajuanController extends Controller {
+    public function getColorStatus($status) {
+        switch($status) {
             case "Diajukan":
                 return "bg-dark-light text-dark pe-none";
                 break;
@@ -55,17 +53,16 @@ class PengajuanController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
+    public function index() {
         Carbon::setLocale('id');
         $menu = Menu::with('submenu')->get();
         $userId = Auth::id();
         //$listPengajuan = Pengadaan::where('user_id', $userId)->get();
         $listPengajuan = Pengadaan::all();
 
-        foreach ($listPengajuan as $pengajuan) {
+        foreach($listPengajuan as $pengajuan) {
             // Pastikan kolom tanggal pengadaan ada dan bukan null
-            if (!empty($pengajuan->tanggal_pengadaan)) {
+            if(!empty($pengajuan->tanggal_pengadaan)) {
                 // Parse tanggal dan ubah formatnya ke 'tanggal bulan(tulisan) tahun'
                 // contoh: '1 Januari 2023'
                 $pengajuan->tanggal_pengadaan_formatted = Carbon::createFromFormat('Y-m-d', $pengajuan->tanggal_pengadaan)
@@ -90,8 +87,7 @@ class PengajuanController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function details($pengadaanId)
-    {
+    public function details($pengadaanId) {
         $menu = Menu::with('submenu')->get();
         $pengadaan = Pengadaan::findOrFail($pengadaanId);
         // Mengambil dokumen pengadaan terkait dengan pengadaan yang dipilih
@@ -109,19 +105,17 @@ class PengajuanController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
-    {
+    public function create() {
         $menu = Menu::with('submenu')->get();
         $roles = Role::all();
 
         return view('dashboard.pengadaan.unit.add', [
             'menu' => $menu,
             'roles' => $roles,
-        ]);  
+        ]);
     }
 
-    public function kirimForm(Request $request)
-    {
+    public function kirimForm(Request $request) {
         $validatedData = Validator::make($request->all(), [
             'role_id' => 'required|numeric',
             'nama_pengadaan' => 'required|string',
@@ -138,7 +132,7 @@ class PengajuanController extends Controller
             'dokumen_memo.mimes' => 'Dokumen harus dalam format DOCX',
         ]);
 
-        if ($validatedData->fails()) {
+        if($validatedData->fails()) {
             return redirect()->back()->withErrors($validatedData)->withInput();
         }
 
@@ -153,14 +147,14 @@ class PengajuanController extends Controller
             $pengajuan->status = "Diajukan";
             $pengajuan->penyelenggara = 3;
 
-            if ($pengajuan->save()) {
-                Log::info('Pengadaan dengan ID: ' . $pengajuan->id . ' berhasil dibuat.');
+            if($pengajuan->save()) {
+                Log::info('Pengadaan dengan ID: '.$pengajuan->id.' berhasil dibuat.');
 
                 $dokumen = new Dokumen;
                 $dokumen->user_id = $userId;
                 $dokumen->pengadaan_id = $pengajuan->id;
-                if ($dokumen->save()) {
-                    Log::info('Dokumen dengan ID: ' . $dokumen->id . ' berhasil dibuat.');
+                if($dokumen->save()) {
+                    Log::info('Dokumen dengan ID: '.$dokumen->id.' berhasil dibuat.');
 
                     $pathKak = $request->file('dokumen_kak')->store('public/dokumen');
                     $dokumenPengadaanKak = new DokumenPengadaan;
@@ -168,7 +162,7 @@ class PengajuanController extends Controller
                     $dokumenPengadaanKak->tipe_dokumen = 'kak';
                     $dokumenPengadaanKak->path_file = $pathKak;
                     $dokumenPengadaanKak->save();
-                    Log::info('Dokumen Pengadaan KAK dengan ID: ' . $dokumenPengadaanKak->id . ' berhasil disimpan.');
+                    Log::info('Dokumen Pengadaan KAK dengan ID: '.$dokumenPengadaanKak->id.' berhasil disimpan.');
 
                     // Menangani unggahan dokumen_memo
                     $pathMemo = $request->file('dokumen_memo')->store('public/dokumen');
@@ -177,27 +171,15 @@ class PengajuanController extends Controller
                     $dokumenPengadaanMemo->tipe_dokumen = 'memo';
                     $dokumenPengadaanMemo->path_file = $pathMemo;
                     $dokumenPengadaanMemo->save();
-                    Log::info('Dokumen Pengadaan Memo dengan ID: ' . $dokumenPengadaanMemo->id . ' berhasil disimpan.');
+                    Log::info('Dokumen Pengadaan Memo dengan ID: '.$dokumenPengadaanMemo->id.' berhasil disimpan.');
 
                     return redirect()->route('unit.pengajuan.index')->with('success', 'Pengajuan pengadaan berhasil disimpan.');
                 }
             }
         } catch (\Exception $e) {
-            Log::error('Kesalahan saat menyimpan data: ' . $e->getMessage());
-            return back()->with('status.error', 'Terjadi kesalahan saat mengirim form: ' . $e->getMessage());
+            Log::error('Kesalahan saat menyimpan data: '.$e->getMessage());
+            return back()->with('status.error', 'Terjadi kesalahan saat mengirim form: '.$e->getMessage());
         }
-    }
-
-    public function downloadTemplate($filename)
-    {
-        $path = 'public/templates-dokumen/' . $filename . '.docx';
-        if (Storage::exists($path)) {
-            Log::info('Download File: ' . $filename . ' berhasil');
-            return Storage::download($path);
-        }
-
-        Log::error('Download File: ' . $filename . '.docx' . ' gagal');
-        abort(404, 'File not found');
     }
 
 
@@ -207,8 +189,7 @@ class PengajuanController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
-    {
+    public function store(Request $request) {
         //
     }
 
@@ -218,8 +199,7 @@ class PengajuanController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
-    {
+    public function show($id) {
         //
     }
 
@@ -229,8 +209,7 @@ class PengajuanController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
-    {
+    public function edit($id) {
         //
     }
 
@@ -241,8 +220,7 @@ class PengajuanController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
-    {
+    public function update(Request $request, $id) {
         //
     }
 
@@ -252,8 +230,7 @@ class PengajuanController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
-    {
+    public function destroy($id) {
         //
     }
 }
